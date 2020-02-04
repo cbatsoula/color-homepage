0672c55acefeb6f7f9ec7a75feb5a5b333db7c4a
cbatsoula <cbatsoula@gmail.com>
2020-02-04 18:20:23 -0500

    working on random background color generator

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  console.log(randomColor)
+
   return (
     <div className="App">
+
+
 
     </div>
   );

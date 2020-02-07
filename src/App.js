ae8ebb4e456f0132a9395da151d84dd7aa85be50
cbatsoula <cbatsoula@gmail.com>
2020-02-06 21:34:13 -0500

    clock added, with each tick of the second hand the background color changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 
 class App extends React.Component {
 
+  state = {
+    date: new Date(),
+  }
+
   randomColor = () => {
     let start = "#"
     let color = Math.floor(Math.random()*16777215).toString(16);
@@ -13,12 +17,43 @@ class App extends React.Component {
 
 
 
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     date: new Date(),
+  //     color: randomColor(),
+  //   };
+  //
+  // }
+
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+
+
+
 
   render () {
 
     return (
       <div className="App" style={{backgroundColor: this.randomColor()}}>
-
+      <h1>Hello, world!</h1>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
 
 
       </div>
